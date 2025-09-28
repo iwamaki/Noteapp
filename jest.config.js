45915acfb81f576f8ca7ddcd3ad02954d746de0b
iwamaki <aramaki1027@gmail.com>
2025-09-28 11:13:35 +0900

    testフォルダ作成

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|uuid)"
-  ]
+  ],
+  testMatch: [
+    "**/src/tests/**/*.test.ts",
+    "**/src/__tests__/**/*.+(ts|tsx|js)?(x)"
+  ],
 };
